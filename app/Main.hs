882ca62904f9b2be950396b642017ae56f39fc1b
mitsuji <mitsuji@na-s.jp>
2018-04-06 09:12:38 +0900

    refactoring

--- app/Main.hs
+++ app/Main.hs
@@ -37,41 +37,48 @@ import Data.List (sortOn)
 -- 0x3093 -- 'ん'
 -- 0x3094 -- 'ゔ'
 
-type WordA          = IOA.IOArray  (Int,Int,Int) (T.Text,T.Text)  -- 単語の配列の配列(index:(先頭,終端,index))
-type WordCountA     = IOA.IOUArray (Int,Int) Int                  -- 未使用単語数の配列(index:(先頭,終端))
+type Word'    = T.Text -- 単語
+type Kana     = T.Text -- 単語の読み(ひらがな)
+type KanaCode = Int    -- かな番
+type Index    = Int 
+type Count    = Int
+
+type WordA       = IOA.IOArray  (KanaCode,KanaCode,Index) (Word',Kana)  -- 単語の配列の配列(index:(先頭,終端,index))
+type WordCountA  = IOA.IOUArray (KanaCode,KanaCode) Count               -- 未使用単語数の配列(index:(先頭,終端))
+type WordRanking = [(KanaCode,Count)]                                   -- 先頭毎の単語数ランキング, (かな番,単語数) のリスト(多い順)
 
 
 main :: IO ()
 main = do
-  wordA      <- (newArray ((0,0,0),(kn 'ゔ',kn 'ゔ',800)) ("","")) :: IO WordA
-  wordCountA <- (newArray ((0,0),(kn 'ゔ',kn 'ゔ')) 0) :: IO WordCountA
+  wordA      <- (newArray ((0,0,0),(kc 'ゔ',kc 'ゔ',800)) ("","")) :: IO WordA
+  wordCountA <- (newArray ((0,0),(kc 'ゔ',kc 'ゔ')) 0) :: IO WordCountA
   headRanks  <- load wordA wordCountA  -- 単語を配列にロード,先頭文字毎のランキングを集計
   shiritori wordA wordCountA headRanks -- しりとりをする
 
 
 
-shiritori :: WordA -> WordCountA -> [(Int,Int)] -> IO ()
-shiritori wordA wordCountA hr = loop hr (kn 'り')
+shiritori :: WordA -> WordCountA -> WordRanking -> IO ()
+shiritori wordA wordCountA hr = loop hr (kc 'り')
   where
     -- しりとりをする
     -- ランキング -> 先頭文字
-    loop :: [(Int,Int)] -> Int -> IO ()
+    loop :: WordRanking -> KanaCode -> IO ()
     loop hr h = do
-      m <- get hr h  -- 次の単語の取得
+      m <- next hr h  -- 次の単語の取得
       case m of
-        Just ((word,kana),l,hr') -> do
+        Just ((word,kana),h',hr') -> do
           T.putStrLn $ word <> "（" <> kana <> "）"
-          loop hr' l
+          loop hr' h'
         Nothing -> return () -- 終了
 
     -- 次の単語の取得とランキング更新
     -- ランキング -> 先頭文字 -> IO (Maybe (単語,終端文字,新ランキング))
-    get :: [(Int,Int)] -> Int -> IO (Maybe ((T.Text,T.Text),Int,[(Int,Int)]))
-    get hr h = f hr >>= (\m -> return $ (\(txt,l) -> (txt,l,(update' hr l))) <$> m)
+    next :: WordRanking -> KanaCode -> IO (Maybe ((Word',Kana),KanaCode,WordRanking))
+    next hr h = f hr >>= (\m -> return $ (\(txt,l) -> (txt,l,(update hr h))) <$> m)
       where
         -- 次の単語の取得
         -- 部分ランキング -> IO (Maybe (単語,終端文字))
-        f :: [(Int,Int)] -> IO (Maybe ((T.Text,T.Text),Int))
+        f :: WordRanking -> IO (Maybe ((Word',Kana),KanaCode))
         f ((l,_):hr') = do
           wc <- readArray wordCountA (h,l) -- 最上ランクの単語が残っているかチェック
           if wc > 0
@@ -84,10 +91,10 @@ shiritori wordA wordCountA hr = loop hr (kn 'り')
                  then return Nothing  -- ランキングが最後に達したら終わり
                  else f hr' -- 次のランクをチェック
 
-        -- 次に使う先頭文字の残数を減らしてランキングを更新
-        -- ランキング -> 次に使う先頭文字 -> 新ランキング
-        update' :: [(Int,Int)] -> Int -> [(Int,Int)]
-        update' hr h =
+        -- 使った先頭文字の残数を減らしてランキングを更新
+        -- ランキング -> 使った先頭文字 -> 新ランキング
+        update :: WordRanking -> KanaCode -> WordRanking
+        update hr h =
           let
             Just c = lookup h hr
             hr' = (h,c-1):(filter (\(h',_)-> h'/=h ) hr)
@@ -95,7 +102,7 @@ shiritori wordA wordCountA hr = loop hr (kn 'り')
 
 
 
-load :: WordA -> WordCountA -> IO [(Int,Int)]
+load :: WordA -> WordCountA -> IO WordRanking
 load wordA wordCountA = do
   loop      -- 単語を配列にロード
   aggregate -- 先頭毎のランキングを集計
@@ -110,41 +117,41 @@ load wordA wordCountA = do
         else do
            line <- T.hGetLine stdin
            let word:kana:_ = T.splitOn "," line
-           let (h,l) = (kn $ head' kana, kn $ last' kana)
+           let (h,l) = (kc $ head' kana, kc $ last' kana)
            c <- readArray wordCountA (h,l)
            writeArray wordA (h,l,c) (word,kana)
            writeArray wordCountA (h,l) (c+1)
            loop
 
     -- ランキングを集計
-    aggregate :: IO [(Int,Int)]
+    aggregate :: IO WordRanking
     aggregate  = do
       ((b,_),(e,_)) <- getBounds wordCountA
       sortOn (Down . snd) <$> mapM headCount [b..e]
       where
-        headCount :: Int -> IO (Int,Int)
+        headCount :: KanaCode -> IO (KanaCode,Count)
         headCount h = do
           ((_,b),(_,e)) <- getBounds wordCountA
           count <- foldM add' 0 [b..e]
           return (h,count)
           where
-            add' :: Int -> Int -> IO Int
+            add' :: Count -> KanaCode -> IO Count
             add' acc l = do
               c <- readArray wordCountA (h,l)
               return $ acc + c
 
 
 
-kn :: Char -> Int
-kn c = fromEnum c - fromEnum 'あ'
+kc :: Char -> KanaCode
+kc c = fromEnum c - fromEnum 'あ'
 
-head' :: T.Text -> Char
+head' :: Kana-> Char
 head' = T.head
 
-last' :: T.Text -> Char
+last' :: Kana -> Char
 last' = capit . f
   where
-    f :: T.Text -> Char
+    f :: Kana -> Char
     f txt = case T.last txt of
       x | False
           || x == 'ー'
